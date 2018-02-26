fc1f6ca5285228595d93fbc55c5b8e1bb8a7bad2
Dimitrijs Fedotovs <dima@fedoto.ws>
2018-02-26 20:39:36 +0200

    homework

--- src/lv/tsi/guessnum/Main.java
+++ src/lv/tsi/guessnum/Main.java
@@ -9,14 +9,32 @@ public class Main {
 
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
+        String repeatAnswer;
+        do {
+            int myNum = random.nextInt(100) + 1;
 
-        int myNum = random.nextInt(100) + 1;
+            System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
+            System.out.println("Spoiler: " + myNum);
 
-        System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
-        System.out.println("Spoiler: " + myNum);
+            for (int i = 1; i <= 10; i++) {
+                System.out.print("Attempt " + i + ". Your guess: ");
+                int userNum = scanner.nextInt();
+                if (userNum > myNum) {
+                    System.out.println("Your number is greater than mine!");
+                } else if (userNum < myNum) {
+                    System.out.println("Your number is less than mine!");
+                } else {
+                    System.out.println("Congrats! You won in " + i + " attempts!");
+                    break;
+                }
+                if (i == 10) {
+                    System.out.println("Sorry, you lost, my friend! The number was " + myNum);
+                }
+            }
+            System.out.print("Do you want to play once more? (yes / no): ");
+            repeatAnswer = scanner.next();
+        } while ("yes".equals(repeatAnswer));
 
-        System.out.print("Your guess:");
-        int userNum = scanner.nextInt();
-
+        System.out.println("Good bye!");
     }
 }
