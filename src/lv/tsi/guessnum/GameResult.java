67e44d8ef34a15f61bd14ba25276d4211fdfd2f5
Dimitrijs Fedotovs <dima@fedoto.ws>
2018-03-05 20:16:11 +0200

    before 6 Feb 2018

---
+++ src/lv/tsi/guessnum/GameResult.java
@@ -0,0 +1,13 @@
+package lv.tsi.guessnum;
+
+/**
+ * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
+ * @version 1.0
+ * @since 1.0
+ */
+public class GameResult {
+    String name;
+    int number;
+    int attempts;
+    boolean winner;
+}
--- src/lv/tsi/guessnum/Main.java
+++ src/lv/tsi/guessnum/Main.java
@@ -1,39 +1,21 @@
 package lv.tsi.guessnum;
 
-import java.util.InputMismatchException;
-import java.util.NoSuchElementException;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
+        ArrayList<GameResult> leaderBoard = new ArrayList<>();
         try {
-            Random random = new Random();
             String repeatAnswer;
             do {
-                int myNum = random.nextInt(100) + 1;
-
-                System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
-                System.out.println("Spoiler: " + myNum);
-
-                boolean userLost = true;
-                for (int i = 1; i <= 10; i++) {
-                    System.out.print("Attempt " + i + ". Your guess: ");
-                    int userNum = askNumber();
-                    if (userNum > myNum) {
-                        System.out.println("Your number is greater than mine!");
-                    } else if (userNum < myNum) {
-                        System.out.println("Your number is less than mine!");
-                    } else {
-                        System.out.println("Congrats! You won in " + i + " attempts!");
-                        userLost = false;
-                        break;
-                    }
-                }
-                if (userLost) {
-                    System.out.println("Sorry, you lost, my friend! The number was " + myNum);
+                System.out.println("What is your name?");
+                String name = scanner.next();
+                GameResult result = doGame(name);
+                if (result.winner) {
+                    leaderBoard.add(result);
                 }
                 System.out.print("Do you want to play once more? (yes / no): ");
                 repeatAnswer = askYesOrNo();
@@ -42,6 +24,41 @@ public class Main {
             System.out.println("Oh! It's pity! You decided to cancel the game!");
         }
         System.out.println("Good bye!");
+        for (GameResult r : leaderBoard) {
+            System.out.println(r.name + "\t" + r.attempts);
+        }
+    }
+
+    private static GameResult doGame(String userName) {
+        int myNum = random.nextInt(100) + 1;
+        System.out.println("Hello, " + userName);
+        System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
+        System.out.println("Spoiler: " + myNum);
+        GameResult result = new GameResult();
+        result.attempts = 10;
+        result.winner = false;
+        result.name = userName;
+        result.number = myNum;
+        boolean userLost = true;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Attempt " + i + ". Your guess: ");
+            int userNum = askNumber();
+            if (userNum > myNum) {
+                System.out.println("Your number is greater than mine!");
+            } else if (userNum < myNum) {
+                System.out.println("Your number is less than mine!");
+            } else {
+                System.out.println("Congrats! You won in " + i + " attempts!");
+                userLost = false;
+                result.winner = true;
+                result.attempts = i;
+                break;
+            }
+        }
+        if (userLost) {
+            System.out.println("Sorry, you lost, my friend! The number was " + myNum);
+        }
+        return result;
     }
 
     static String askYesOrNo() {
