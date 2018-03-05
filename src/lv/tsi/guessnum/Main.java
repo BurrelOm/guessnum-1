2fd30e9b57bd8405798cb99c2ecfe6d1b801dc3e
Dimitrijs Fedotovs <dima@fedoto.ws>
2018-03-05 18:48:33 +0200

    homework after lesson 27 Feb

--- src/lv/tsi/guessnum/Main.java
+++ src/lv/tsi/guessnum/Main.java
@@ -1,6 +1,7 @@
 package lv.tsi.guessnum;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,37 +9,49 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        try {
+            Random random = new Random();
+            String repeatAnswer;
+            do {
+                int myNum = random.nextInt(100) + 1;
 
-        Random random = new Random();
-        String repeatAnswer;
-        do {
-            int myNum = random.nextInt(100) + 1;
+                System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
+                System.out.println("Spoiler: " + myNum);
 
-            System.out.println("I'm thinking of number from 1 to 100. Try to guess it!");
-            System.out.println("Spoiler: " + myNum);
-
-            boolean userLost = true;
-            for (int i = 1; i <= 10; i++) {
-                System.out.print("Attempt " + i + ". Your guess: ");
-                int userNum = askNumber();
-                if (userNum > myNum) {
-                    System.out.println("Your number is greater than mine!");
-                } else if (userNum < myNum) {
-                    System.out.println("Your number is less than mine!");
-                } else {
-                    System.out.println("Congrats! You won in " + i + " attempts!");
-                    userLost = false;
-                    break;
+                boolean userLost = true;
+                for (int i = 1; i <= 10; i++) {
+                    System.out.print("Attempt " + i + ". Your guess: ");
+                    int userNum = askNumber();
+                    if (userNum > myNum) {
+                        System.out.println("Your number is greater than mine!");
+                    } else if (userNum < myNum) {
+                        System.out.println("Your number is less than mine!");
+                    } else {
+                        System.out.println("Congrats! You won in " + i + " attempts!");
+                        userLost = false;
+                        break;
+                    }
                 }
-            }
-            if (userLost) {
-                System.out.println("Sorry, you lost, my friend! The number was " + myNum);
-            }
-            System.out.print("Do you want to play once more? (yes / no): ");
-            repeatAnswer = scanner.next();
-        } while ("yes".equals(repeatAnswer));
-
+                if (userLost) {
+                    System.out.println("Sorry, you lost, my friend! The number was " + myNum);
+                }
+                System.out.print("Do you want to play once more? (yes / no): ");
+                repeatAnswer = askYesOrNo();
+            } while ("yes".equalsIgnoreCase(repeatAnswer));
+        } catch (NoSuchElementException e) {
+            System.out.println("Oh! It's pity! You decided to cancel the game!");
+        }
         System.out.println("Good bye!");
+    }
+
+    static String askYesOrNo() {
+        for (; ; ) {
+            String answer = scanner.next();
+            if ("YES".equalsIgnoreCase(answer) || "NO".equalsIgnoreCase(answer)) {
+                return answer;
+            }
+            System.out.print("Answers \"yes\" or \"no\" are only acceptable! Try again: ");
+        }
     }
 
     static int askNumber() {
@@ -51,7 +64,7 @@ public class Main {
                 System.out.print("Wrong number! It should be a number between 1 and 100. Try again: ");
             } catch (InputMismatchException e) {
                 System.out.print("Wrong input! It should be a number! Try again: ");
-                scanner.next();
+                askYesOrNo();
             }
         }
     }
